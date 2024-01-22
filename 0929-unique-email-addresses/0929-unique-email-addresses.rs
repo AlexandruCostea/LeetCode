c9e04c8b3854b2ffafc06ae9847fdde7ff729535
Costea Sigartău Alexandru <115140116+AlexandruCostea@users.noreply.github.com>
2024-01-22 20:35:15 +0200

    Time: 2 ms (88%), Space: 2.2 MB (92%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.rs
+++ 0929-unique-email-addresses/0929-unique-email-addresses.rs
@@ -4,14 +4,14 @@ impl Solution {
     pub fn num_unique_emails(emails: Vec<String>) -> i32 {
         let mut set = HashSet::new();
         for mail in emails {
-            let x: Vec<String> = mail.split("@").map(|x| x.to_string()).collect();
+            let x : Vec<&str> = mail.split("@").collect();
             let receiver:Vec<&str> = x[0].split("+").collect();
             let receiver:String = String::from_utf8(
                 receiver[0].as_bytes().iter()
                     .map(|&x| {x})
                     .filter(|&x| {x != b'.'})
                     .collect()).unwrap();
-            let mail = receiver + "@" + x[1].as_str();
+            let mail = receiver + "@" + x[1];
             set.insert(mail);
         }
         return set.len() as i32;
