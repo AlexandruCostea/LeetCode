3488a1d61d4b246daef4e55b2d74f2d2f17d4eba
Costea Sigartău Alexandru <115140116+AlexandruCostea@users.noreply.github.com>
2024-01-23 17:51:04 +0200

    Time: 0 ms (100%), Space: 2.4 MB (44.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.rs
+++ 0125-valid-palindrome/0125-valid-palindrome.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
         let text:String = s.to_lowercase().split(|c: char| {!c.is_alphanumeric()}).collect();
-        let mut reverse = text.clone().as_bytes().to_owned();
+        let mut reverse = text.as_bytes().to_owned();
         reverse.reverse();
         let reverse = String::from_utf8(reverse.to_vec()).unwrap();
 
