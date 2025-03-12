53160085eec8ec3ddcd65516acf635bddd86a43a
Costea Sigartău Alexandru <115140116+AlexandruCostea@users.noreply.github.com>
2025-03-12 20:32:26 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.rs
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.rs
@@ -3,26 +3,47 @@ use std::collections::HashMap;
 impl Solution {
     pub fn number_of_substrings(s: String) -> i32 {
         let mut sol: usize = 0;
-        let chars: Vec<char> = s.chars().collect();
-        let mut letters: HashMap<char, i32> = HashMap::new();
+        let word: Vec<char> = s.chars().collect();
+        let mut frequency: HashMap<char, i32> = HashMap::new();
         let mut i: usize = 0;
         let mut j: usize = 0;
-        letters.insert('a', 0);
-        letters.insert('b', 0);
-        letters.insert('c', 0);
-        while i < chars.len() {
-            while (*letters.get(&'a').unwrap() == 0 || *letters.get(&'b').unwrap() == 0 || *letters.get(&'c').unwrap() == 0) && j< chars.len() {
-                *letters.entry(chars[j]).or_default() += 1;
+        let mut prevj: usize = 0;
+        let mut previ: usize = 0;
+
+        frequency.insert('a', 0);
+        frequency.insert('b', 0);
+        frequency.insert('c', 0);
+
+        while i < word.len() {
+            println!("{j}");
+            while (*frequency.get(&'a').unwrap() == 0 || *frequency.get(&'b').unwrap() == 0 || *frequency.get(&'c').unwrap() == 0) && j < word.len() {
+                *frequency.entry(word[j]).or_default() += 1;
                 j += 1;
             }
-            if (*letters.get(&'a').unwrap() == 0 || *letters.get(&'b').unwrap() == 0 || *letters.get(&'c').unwrap() == 0) && j == chars.len(){
-                break;
-            }
-            sol += chars.len() - j + 1;
-            *letters.entry(chars[i]).or_default() -= 1;
-            i +=1;
-        }
 
-        (sol) as i32
+            j -= 1;
+            
+            println!("{i} {j} {:?}", frequency);
+
+            while *frequency.get(&'a').unwrap() > 0 && *frequency.get(&'b').unwrap() > 0 && *frequency.get(&'c').unwrap() > 0 {
+                *frequency.entry(word[i]).or_default() -= 1;
+                i += 1;
+
+                if *frequency.get(&'a').unwrap() > 0 && *frequency.get(&'b').unwrap() > 0 && *frequency.get(&'c').unwrap() > 0 {
+                    sol += word.len() - j;
+                }
+            }
+            println!("{i} {j} {:?}\n", frequency);
+            j+= 1;
+
+            if i == previ && j == prevj {
+                return sol as i32;
+            }
+
+            sol += word.len() - j + 1;
+            previ = i;
+            prevj = j;
+        }
+        sol as i32
     }
 }
